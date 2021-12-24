db1ec9560c4a4f1587639a3e75f192a822d1a35b
Luke <manhnvhanu@outlook.com>
2021-12-25 02:27:09 +0700

    Solve Day 2: Counting Sort 1

--- src/main/kotlin/one_week_reparation_kit/day_02/CountingSort1.kt
+++ src/main/kotlin/one_week_reparation_kit/day_02/CountingSort1.kt
@@ -1,2 +1,22 @@
 package one_week_reparation_kit.day_02
 
+/*
+ * Complete the 'countingSort' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+fun countingSort(arr: Array<Int>): Array<Int> {
+    // Write your code here
+    val size = arr.size
+    val result = IntArray(size)
+
+    for (i in 0 until size) {
+        val counter = result[arr[i]]
+        if (counter == 0) result[arr[i]] = 1
+        else result[arr[i]]++
+    }
+
+    return result.take(100).toTypedArray()
+}
