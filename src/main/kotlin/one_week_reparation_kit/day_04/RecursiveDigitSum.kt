dabeb3a616c1fcf0691176e60248bb3242d4a2af
Luke <manhnvhanu@outlook.com>
2021-12-26 01:46:12 +0700

    Solve Day 4 - Recursive Digit Sum

--- src/main/kotlin/one_week_reparation_kit/day_04/RecursiveDigitSum.kt
+++ src/main/kotlin/one_week_reparation_kit/day_04/RecursiveDigitSum.kt
@@ -1,2 +1,21 @@
 package one_week_reparation_kit.day_04
 
+/*
+ * Complete the 'superDigit' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING n
+ *  2. INTEGER k
+ */
+
+fun superDigit(n: String, k: Int): Int {
+    if (n.length == 1) return n.toInt()
+    var sum = 0L
+
+    for (i in n.indices) {
+        sum += n[i].toString().toInt() * k
+    }
+
+    return if (sum > 10) superDigit(sum.toString(), 1) else sum.toInt()
+}
