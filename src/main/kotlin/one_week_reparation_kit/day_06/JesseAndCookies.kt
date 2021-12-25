0a750817dfbac9023a80841d7ee836b7af037f5e
Luke <manhnvhanu@outlook.com>
2021-12-26 03:35:35 +0700

    Solve Day 6 - Jesse and Cookies

--- src/main/kotlin/one_week_reparation_kit/day_06/JesseAndCookies.kt
+++ src/main/kotlin/one_week_reparation_kit/day_06/JesseAndCookies.kt
@@ -1,2 +1,23 @@
 package one_week_reparation_kit.day_06
 
+import java.util.*
+
+/*
+ * Complete the 'cookies' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY A
+ */
+
+fun cookies(k: Int, A: Array<Int>): Int {
+    val sortedSweetnessValues = PriorityQueue<Int>()
+    sortedSweetnessValues.addAll(A)
+    var numerOfoperations = 0
+    while (sortedSweetnessValues.peek() < k && sortedSweetnessValues.size > 1) {
+        ++numerOfoperations
+        sortedSweetnessValues.add(sortedSweetnessValues.poll() + sortedSweetnessValues.poll() * 2)
+    }
+    return if (sortedSweetnessValues.first() >= k) numerOfoperations else -1
+}
