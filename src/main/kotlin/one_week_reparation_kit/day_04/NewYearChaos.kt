81e220dd1ffaa4ded24066cdf5b399e01193e187
Luke <manhnvhanu@outlook.com>
2021-12-26 02:12:25 +0700

    Solve Day 4: New Year Chaos

--- src/main/kotlin/one_week_reparation_kit/day_04/NewYearChaos.kt
+++ src/main/kotlin/one_week_reparation_kit/day_04/NewYearChaos.kt
@@ -1,2 +1,23 @@
 package one_week_reparation_kit.day_04
 
+/*
+ * Complete the 'minimumBribes' function below.
+ *
+ * The function accepts INTEGER_ARRAY q as parameter.
+ */
+
+
+//Version 1: Native approach using nested loop
+fun minimumBribes(q: Array<Int>): Unit {
+    var minimumBribes = 0
+    for (i in q.size - 1 downTo 0) {
+        if (q[i] - (i + 1) > 2) {
+            println("Too chaotic")
+            return
+        }
+        for (j in maxOf(0, q[i] - 2) until i)
+            if (q[j] > q[i])
+                minimumBribes++
+    }
+    println(minimumBribes)
+}
